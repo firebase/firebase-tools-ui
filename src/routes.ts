a20dc30e558a7289d16b213a8e4293c8bfefd227
Tim Stirrat <tstirrat@google.com>
2020-03-23 20:22:58 -0700

    Add styleguide route (hidden)

--- src/routes.ts
+++ src/routes.ts
@@ -14,6 +14,7 @@
  * limitations under the License.
  */
 
+import { Components } from './components/common/styleguide/Components';
 import Database from './components/Database';
 import Firestore from './components/Firestore';
 import Home from './components/Home';
@@ -47,5 +48,12 @@ export const routes: ReadonlyArray<Route> = [
     label: 'Firestore',
     exact: false,
     showInNav: true,
+  },
+  {
+    path: '/components',
+    component: Components,
+    label: 'Components',
+    exact: false,
+    showInNav: false,
   },
 ];
