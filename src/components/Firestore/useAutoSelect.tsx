f96fd3b8e31c3735fda4b12f4c1aaaf5fc720fb3
Tim Stirrat <tstirrat@google.com>
2020-03-26 12:16:15 -0700

    return null which is ReactNode compatible

--- src/components/Firestore/useAutoSelect.tsx
+++ src/components/Firestore/useAutoSelect.tsx
@@ -41,9 +41,7 @@ export function useAutoSelect<T extends { id: string }>(list?: T[] | null) {
     setAutoSelect(
       shouldAutoSelect && firstChild ? (
         <Redirect to={`${url}/${firstChild.id}`} />
-      ) : (
-        undefined
-      )
+      ) : null
     );
   }, [pathname, url, list, setAutoSelect]);
 
