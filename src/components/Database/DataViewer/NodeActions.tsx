524cb8f2c8dedb5f8386511d86637fd351ee9806
Tim Stirrat <tstirrat@google.com>
2020-03-27 14:03:51 -0700

    Fix menus to use auto width (#90)

--- src/components/Database/DataViewer/NodeActions.tsx
+++ src/components/Database/DataViewer/NodeActions.tsx
@@ -16,7 +16,7 @@
 
 import { Icon } from '@rmwc/icon';
 import { IconButton } from '@rmwc/icon-button';
-import { Menu, MenuItem, MenuSurfaceAnchor } from '@rmwc/menu';
+import { MenuItem, SimpleMenu } from '@rmwc/menu';
 import { Tooltip } from '@rmwc/tooltip';
 import * as React from 'react';
 import { useState } from 'react';
@@ -131,24 +131,33 @@ export const NodeActions = React.memo<Props>(function NodeActions$({
           />
         </Tooltip>
       ) : null}
-      <MenuSurfaceAnchor>
-        <Menu open={isMenuOpen} onClose={() => setMenuOpen(false)}>
-          <MenuItem onClick={removeNode}>
-            <Icon icon="delete" /> Remove
+      <SimpleMenu
+        hoistToBody={
+          /*
+           * Actions show on hover, so hoisting has some weird effects, popping
+           * in and out of the top left corner
+           */
+          false
+        }
+        handle={<IconButton icon="more_vert" />}
+        onOpen={() => setMenuOpen(true)}
+        onClose={() => setMenuOpen(false)}
+      >
+        <MenuItem onClick={removeNode}>
+          <Icon icon="delete" /> Remove
+        </MenuItem>
+        {!isRoot && (
+          <MenuItem onClick={() => setCloneDialogIsOpen(true)}>
+            <Icon icon="file_copy" /> Clone
           </MenuItem>
-          {!isRoot && (
-            <MenuItem onClick={() => setCloneDialogIsOpen(true)}>
-              <Icon icon="file_copy" /> Clone
-            </MenuItem>
-          )}
-          {!isRoot && (
-            <MenuItem onClick={() => setRenameDialogIsOpen(true)}>
-              <Icon icon="edit" /> Rename
-            </MenuItem>
-          )}
-        </Menu>
-        <IconButton icon="more_vert" onClick={() => setMenuOpen(!isMenuOpen)} />
-      </MenuSurfaceAnchor>
+        )}
+        {!isRoot && (
+          <MenuItem onClick={() => setRenameDialogIsOpen(true)}>
+            <Icon icon="edit" /> Rename
+          </MenuItem>
+        )}
+      </SimpleMenu>
+
       {/* Extra UI that shows when actions are triggered */}
       <div className="NodeActions__additional-ui">
         {isAdding && (
