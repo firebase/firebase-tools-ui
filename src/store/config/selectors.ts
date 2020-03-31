363eb3b290b4f758dff17e758cbd043518bad48d
Yuchen Shi <yuchenshi@google.com>
2020-03-31 13:21:24 -0700

    Use Result in selectors wherever possible.

--- src/store/config/selectors.ts
+++ src/store/config/selectors.ts
@@ -1,18 +1,27 @@
 import { createSelector } from 'reselect';
 
-import { mapResult } from '../utils';
+import { map } from '../utils';
 import { AppState } from '..';
 
+// Get the RemoteResult wrapper for config. If you only care about result,
+// use getConfigResult instead for better caching.
 export const getConfig = (state: AppState) => state.config;
 
-export const getProjectId = createSelector(getConfig, result =>
-  mapResult(result, config => config.projectId)
+export const getConfigResult = (state: AppState) => state.config.result;
+
+// Get whether config is being loaded / refreshed. Note: To check whether config
+// is present, use getConfigResult. This only tells whether we're fetching.
+export const getConfigLoading = (state: AppState) => state.config.loading;
+
+export const getProjectIdResult = createSelector(getConfigResult, result =>
+  map(result, config => config.projectId)
 );
 
-export const getDatabaseConfig = createSelector(getConfig, result =>
-  mapResult(result, config => config.database)
+export const getDatabaseConfigResult = createSelector(getConfigResult, result =>
+  map(result, config => config.database)
 );
 
-export const getFirestoreConfig = createSelector(getConfig, result =>
-  mapResult(result, config => config.firestore)
+export const getFirestoreConfigResult = createSelector(
+  getConfigResult,
+  result => map(result, config => config.firestore)
 );
