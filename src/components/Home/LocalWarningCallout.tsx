df38045c02c2c4a746b4862c6585b04fffc86dfb
Tim Stirrat <tstirrat@google.com>
2020-03-31 14:54:34 -0700

    Add a dismissable warning about local environment

---
+++ src/components/Home/LocalWarningCallout.tsx
@@ -0,0 +1,44 @@
+import { useLocalStorage, writeStorage } from '@rehooks/local-storage';
+import { Button } from '@rmwc/button';
+import { GridCell } from '@rmwc/grid';
+import React from 'react';
+
+import { Callout } from '../common/Callout';
+
+const CONSOLE_ROOT = 'https://console.firebase.google.com';
+const DISMISS_KEY = 'isDismissed';
+
+export const LocalWarningCallout: React.FC<{
+  projectId: string;
+}> = ({ projectId }) => {
+  const [isDismissed] = useLocalStorage<boolean>(DISMISS_KEY);
+
+  if (isDismissed) {
+    return null;
+  }
+
+  return (
+    <GridCell span={12}>
+      <Callout
+        type="note"
+        actions={
+          <>
+            <Button
+              tag="a"
+              href={`${CONSOLE_ROOT}/project/${projectId}/overview`}
+              target="_blank"
+              label="View project"
+            />
+            <Button
+              label="Dismiss"
+              onClick={() => writeStorage(DISMISS_KEY, true)}
+            />
+          </>
+        }
+      >
+        Remember this is a local enivornment. Visit the production version of{' '}
+        <strong>{projectId}</strong> in the console.
+      </Callout>
+    </GridCell>
+  );
+};
