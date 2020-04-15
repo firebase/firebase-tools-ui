9a1915cf054e92e00a544b927e96dd3db3615c9d
Tim Stirrat <tstirrat@google.com>
2020-04-15 16:11:34 -0700

    Allow adding field to create a missing doc

--- src/components/Firestore/DocumentPreview/index.tsx
+++ src/components/Firestore/DocumentPreview/index.tsx
@@ -16,6 +16,7 @@
 
 import { Button } from '@rmwc/button';
 import { List, ListItem } from '@rmwc/list';
+import { Typography } from '@rmwc/typography';
 import { firestore } from 'firebase';
 import React, { useState } from 'react';
 import { useDocumentData } from 'react-firebase-hooks/firestore';
@@ -36,16 +37,15 @@ const DocumentPreview: React.FC<Props> = ({
   reference,
   maxSummaryLen = 20,
 }) => {
-  const [data, loading, error] = useDocumentData(reference);
+  const [data, loading, error] = useDocumentData<FirestoreMap>(reference);
   const [isAddingField, setIsAddingField] = useState(false);
 
   if (loading) return <div>Loading document...</div>;
   if (error) return <div>Error retrieving document</div>;
-  if (!data) return <div>Error resolving document</div>;
 
   return (
     <>
-      <DocumentProvider value={data as FirestoreMap}>
+      <DocumentProvider value={data || {}}>
         <List
           dense
           className="List-Actions Firestore-Document-Fields"
@@ -67,14 +67,18 @@ const DocumentPreview: React.FC<Props> = ({
                 setIsAddingField(false);
               }}
               onSave={(key, value) => {
-                updateField(reference, data as FirestoreMap, [key], value);
+                if (!data) {
+                  reference.set({ [key]: value });
+                } else {
+                  updateField(reference, data, [key], value);
+                }
                 setIsAddingField(false);
               }}
               areRootKeysMutable={true}
             />
           )}
 
-          {isMap(data) && (
+          {data && isMap(data) ? (
             <div className="Firestore-Field-List">
               {Object.keys(data).map(name => (
                 <FieldPreview
@@ -85,6 +89,17 @@ const DocumentPreview: React.FC<Props> = ({
                 />
               ))}
             </div>
+          ) : (
+            <Typography
+              theme="secondary"
+              use="body2"
+              className="Firestore-Document-Warning"
+            >
+              <i>
+                This document does not exist, it will not appear in queries or
+                snapshots
+              </i>
+            </Typography>
           )}
         </List>
       </DocumentProvider>
