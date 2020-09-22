1fe8fe1b770724846acfc798f00f157bf2907a40
kirjs <kirjs@google.com>
2020-09-21 20:59:41 -0400

    fix(auth): Empty state of table styling. Should be secondary color text and the same size as table font.

--- src/components/Auth/UsersCard/table/AuthZeroState.tsx
+++ src/components/Auth/UsersCard/table/AuthZeroState.tsx
@@ -1,9 +1,12 @@
+import { Typography } from '@rmwc/typography';
 import React from 'react';
 
 import styles from './UsersTable.module.scss';
 
 export const AuthZeroState: React.FC = () => {
   return (
-    <div className={styles.zeroStateWrapper}>No users for this project yet</div>
+    <Typography use="body2" className={styles.zeroStateWrapper}>
+      No users for this project yet
+    </Typography>
   );
 };
--- src/components/Auth/UsersCard/table/NoResults.tsx
+++ src/components/Auth/UsersCard/table/NoResults.tsx
@@ -1,12 +1,16 @@
+import { Typography } from '@rmwc/typography';
 import React from 'react';
 
 import styles from './UsersTable.module.scss';
 
 export const NoResults: React.FC = () => {
-  // TODO(kirjs): implement the actual no results state.
   return (
-    <div aria-live="polite" className={styles.noResultsWrapper}>
+    <Typography
+      use="body2"
+      aria-live="polite"
+      className={styles.noResultsWrapper}
+    >
       No results
-    </div>
+    </Typography>
   );
 };
