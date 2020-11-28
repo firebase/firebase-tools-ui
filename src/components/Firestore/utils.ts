3d7ed18e13e8f166ffff89ae07fb3975b2fdb185
tanabee <tanabee1986@gmail.com>
2020-11-28 09:08:31 +0900

    Show filtered values when sorted in the Firestore emulator UI

--- src/components/Firestore/utils.ts
+++ src/components/Firestore/utils.ts
@@ -136,3 +136,76 @@ export const withFieldRemoved = produce((draft, path: string[]) => {
     parent.splice(Number(lastFieldName(path)), 1);
   }
 });
+
+// Give a brief text summary of the data.
+// Note: maxLen is soft (at least for now). End result may still be longer.
+export function summarize(data: FirestoreAny, maxLen: number): string {
+  switch (getFieldType(data)) {
+    case FieldType.ARRAY:
+      return summarizeArray(data as FirestoreAny[], maxLen);
+    case FieldType.MAP:
+      return summarizeMap(data as Record<string, FirestoreAny>, maxLen);
+    case FieldType.BLOB:
+      const base64 = (data as firestore.Blob).toBase64();
+      if (base64.length < maxLen) return base64;
+      else return base64.substr(0, maxLen) + '...';
+    case FieldType.BOOLEAN:
+      return (data as boolean).toString();
+    case FieldType.GEOPOINT:
+      const value = data as firestore.GeoPoint;
+      return `[${latStr(value.latitude)}, ${longStr(value.longitude)}]`;
+    case FieldType.NULL:
+      return 'null';
+    case FieldType.NUMBER:
+      return (data as number).toString();
+    case FieldType.REFERENCE:
+      return (data as firestore.DocumentReference).path;
+    case FieldType.STRING:
+      return `"${data as string}"`;
+    case FieldType.TIMESTAMP:
+      // TODO: Better date time formatting.
+      // Note: Not using toLocaleString() since it does not stringify timezone.
+      return (data as firestore.Timestamp).toDate().toString();
+    case FieldType.JSON:
+      throw new Error('JSON field type is input only');
+  }
+}
+
+function summarizeArray(array: FirestoreAny[], maxLen: number): string {
+  let output = '[';
+  for (const element of array) {
+    if (output.length > 1) output += ', ';
+    if (output.length > maxLen) {
+      output += '...';
+      break;
+    }
+    output += summarize(element, maxLen - output.length);
+  }
+  output += ']';
+  return output;
+}
+
+function summarizeMap(
+  map: Record<string, FirestoreAny>,
+  maxLen: number
+): string {
+  let output = '{';
+  for (const [key, value] of Object.entries(map)) {
+    if (output.length > 1) output += ', ';
+    if (output.length > maxLen) {
+      output += '...';
+      break;
+    }
+    output += `${key}: ${summarize(value, maxLen - output.length)}`;
+  }
+  output += '}';
+  return output;
+}
+
+function latStr(lat: number): string {
+  return `${Math.abs(lat)}° ${lat >= 0 ? 'N' : 'S'}`;
+}
+
+function longStr(long: number): string {
+  return `${Math.abs(long)}° ${long >= 0 ? 'E' : 'W'}`;
+}
