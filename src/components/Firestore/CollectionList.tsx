afa025d3af9530a2c4fd4f1646aea65c796eac71
AntonioRdzNav <rodantonio@google.com>
2020-12-01 18:44:08 -0600

    Encoded and decoded Collection Ids as well, and added Unit Test to test useSubCollections hook

--- src/components/Firestore/CollectionList.tsx
+++ src/components/Firestore/CollectionList.tsx
@@ -65,11 +65,19 @@ const CollectionList: React.FC<Props> = ({ collections, reference }) => {
       const ref = reference || firestore;
       const newCollection = ref.collection(value.collectionId);
       await newCollection.doc(value.document.id).set(value.document.data);
+
       // Redirect to the new collection
+      const encodedCollectionId = encodeURIComponent(value.collectionId);
       if (reference) {
-        history.push(`/firestore/${reference.path}/${value.collectionId}`);
+        const encodedReferencePath = reference.path
+          .split('/')
+          .map(uri => encodeURIComponent(uri))
+          .join('/');
+        history.push(
+          `/firestore/${encodedReferencePath}/${encodedCollectionId}`
+        );
       } else {
-        history.push(`/firestore/${value.collectionId}`);
+        history.push(`/firestore/${encodedCollectionId}`);
       }
     }
   };
@@ -104,7 +112,7 @@ const CollectionList: React.FC<Props> = ({ collections, reference }) => {
               key={coll.id}
               className="Firestore-List-Item"
               tag={NavLink}
-              to={`${url}/${coll.id}`}
+              to={`${url}/${encodeURIComponent(coll.id)}`}
               activeClassName="mdc-list-item--activated"
             >
               {coll.id}
