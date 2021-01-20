255d19cfb73c9e41365e07b83e88e21527bbd94e
AntonioRdzNav <rodantonio@google.com>
2021-01-20 10:48:28 -0600

    Removed formatTimestamp utils function and replaced it with one-line date function

--- src/components/Firestore/Requests/RequestsCard/Table/TableRow.tsx
+++ src/components/Firestore/Requests/RequestsCard/Table/TableRow.tsx
@@ -23,7 +23,6 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 
 import { CustomThemeProvider } from '../../../../../themes';
-import { formatTimestamp } from '../../../../../utils';
 import { FirestoreRulesEvaluation } from '../../rules_evaluation_result_model';
 import { OutcomeData } from '../../types';
 import { OUTCOME_DATA } from '../../utils';
