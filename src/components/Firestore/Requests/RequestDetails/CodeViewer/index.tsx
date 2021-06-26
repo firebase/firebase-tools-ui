85e110fd14a2daa209a4ad15b954326cd50a45c8
Yuchen Shi <yuchenshi@google.com>
2021-06-25 20:22:28 -0700

    Improve syntax highlighting.

--- src/components/Firestore/Requests/RequestDetails/CodeViewer/index.tsx
+++ src/components/Firestore/Requests/RequestDetails/CodeViewer/index.tsx
@@ -15,18 +15,21 @@
  */
 
 import 'codemirror/keymap/sublime';
-import 'codemirror/theme/xq-light.css';
+import 'codemirror/lib/codemirror.css';
 
 import './index.scss';
 
-import CodeMirror from '@uiw/react-codemirror';
-import React, { useEffect, useState } from 'react';
+import CodeMirror from 'codemirror';
+import React, { useEffect, useRef } from 'react';
 
 import { textBlackTertiary } from '../../../../../colors';
 import { errorTheme, successTheme } from '../../../../../themes';
 import { OutcomeInfo, RulesOutcome } from '../../rules_evaluation_result_model';
 import { ICON_SELECTOR } from '../../utils';
 import CodeViewerAdminRequest from './AdminRequest';
+import { MODE_FIREBASE_RULES, defineFirebaseRulesMode } from './mode';
+
+// import CodeMirror from '@uiw/react-codemirror';
 
 // TODO: find a way to remove the CSSProperties cast
 // (TypeScript complains if object is not casted)
@@ -38,12 +41,14 @@ const CODE_VIEWER_CUSTOM_STYLES = {
   '--emulator-ui-code-comment-text': textBlackTertiary,
 } as React.CSSProperties;
 
-const CODE_MIRROR_OPTIONS = {
-  theme: 'xq-light',
-  keyMap: 'sublime',
-  mode: 'jsx',
+const CODE_MIRROR_OPTIONS: CodeMirror.EditorConfiguration = {
   tabSize: 2,
-  readOnly: true,
+  indentUnit: 2,
+  lineNumbers: true,
+  theme: 'devsite-light',
+  keyMap: 'sublime',
+  mode: MODE_FIREBASE_RULES,
+  readOnly: 'nocursor',
   gutters: ['CodeMirror-gutter-elt'],
 };
 
@@ -66,48 +71,48 @@ const RulesCodeViewer: React.FC<Props> = ({
   linesOutcome,
   isAdminRequest,
 }) => {
-  const [
-    codeMirrorEditor,
-    setCodeMirrorEditor,
-  ] = useState<CodeMirror.Editor | null>(null);
+  useEffect(defineFirebaseRulesMode, []);
 
-  // Highlights and adds the proper outcome-gutter to the corresponding lines of
-  // the rules code-viewer.
-  // NOTE: a gutter in CodeMirror is an icon displayed next to the line number
-  // NOTE: this current version of the code won't remove old gutters, nor old highlighted-lines
-  // if the (linesOutcome) array were to change, however, this code is fine because the
-  // (codeMirrorEditor) instance is destroyed / created after the component unmounts / mounts
-  // (therefore reseting all gutters and highlighted-lines). Additionally, there is currently
-  // no way to update the linesOutcome array of an existing request.
+  const ref = useRef<HTMLTextAreaElement>(null);
   useEffect(() => {
-    if (codeMirrorEditor && linesOutcome) {
-      linesOutcome.forEach(({ line, outcome }) => {
+    if (ref.current) {
+      if (ref.current.style.display === 'none') {
+        // CodeMirror has already initialized and replaced this textarea.
+        return;
+      }
+      // CodeMirror objects don't need to be destroyed manually and will be
+      // collected by GC once the DOM node is removed.
+      const cm = CodeMirror.fromTextArea(ref.current, CODE_MIRROR_OPTIONS);
+      cm.setSize('100%', '100%');
+      // Highlights and adds the proper outcome-gutter to the corresponding lines of
+      // the rules code-viewer.
+      // NOTE: a gutter in CodeMirror is an icon displayed next to the line number
+      // NOTE: this current version of the code won't remove old gutters, nor old highlighted-lines
+      // if the (linesOutcome) array were to change, however, this code is fine because the
+      // (codeMirrorEditor) instance is destroyed / created after the component unmounts / mounts
+      // (therefore reseting all gutters and highlighted-lines). Additionally, there is currently
+      // no way to update the linesOutcome array of an existing request.
+      linesOutcome?.forEach(({ line, outcome }) => {
         const rulesOutcome: RulesOutcome = outcome ? 'allow' : 'deny';
-        codeMirrorEditor.setGutterMarker(
+        cm.setGutterMarker(
           line - 1,
           'CodeMirror-gutter-elt',
           getMarkerElement(rulesOutcome)
         );
-        codeMirrorEditor.addLineClass(
+        cm.addLineClass(
           line - 1,
           '',
           `highlighted-line-of-code--${rulesOutcome}`
         );
       });
     }
-  }, [codeMirrorEditor, linesOutcome]);
+  }, [ref, firestoreRules, linesOutcome]);
 
   const renderCodeViewer = () => {
     if (isAdminRequest) {
       return <CodeViewerAdminRequest />;
     }
-    return (
-      <CodeMirror
-        value={firestoreRules}
-        options={CODE_MIRROR_OPTIONS}
-        onChanges={(editor) => !codeMirrorEditor && setCodeMirrorEditor(editor)}
-      />
-    );
+    return <textarea ref={ref} value={firestoreRules} readOnly></textarea>;
   };
 
   return (
