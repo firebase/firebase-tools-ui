3f8d38f18e8e122a94aba0f62feaf7b937e51528
Jeff Huleatt <3759507+jhuleatt@users.noreply.github.com>
2021-11-02 10:43:47 -0400

    fetch and modify rc templates

---
+++ src/components/RemoteConfig/api.ts
@@ -0,0 +1,51 @@
+import { RemoteConfigTemplate } from 'firebase-admin/remote-config';
+
+/**
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { useConfig, useEmulatorConfig } from '../common/EmulatorConfigProvider';
+import { useRequest } from '../common/useRequest';
+
+function useRemoteConfigAdminUrl() {
+  const { projectId } = useConfig();
+  const config = useEmulatorConfig('remote config');
+  return `http://${config.hostAndPort}/v1/projects/${projectId}/remoteConfig`;
+}
+
+export function useTemplate(): {
+  template: RemoteConfigTemplate;
+  fetchNewTemplate: () => void;
+} {
+  const url = useRemoteConfigAdminUrl();
+  const { data: template, mutate: fetchNewTemplate } = useRequest(
+    url,
+    {},
+    // disable auto refresh
+    { refreshInterval: 0 }
+  );
+  return { template: template as RemoteConfigTemplate, fetchNewTemplate };
+}
+
+export function useTemplateUpdater() {
+  const url = useRemoteConfigAdminUrl();
+
+  return async function updateTemplate(newTemplate: Object) {
+    const response = await fetch(url, {
+      method: 'PUT',
+      body: JSON.stringify(newTemplate),
+    });
+    return response.ok;
+  };
+}
--- src/components/RemoteConfig/index.tsx
+++ src/components/RemoteConfig/index.tsx
@@ -1,5 +1,37 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { Suspense, useEffect, useMemo, useState } from 'react';
 
-export default function RemoteConfig() {
-  return <h1>Remote Config</h1>;
+import { useTemplate, useTemplateUpdater } from './api';
+
+function RemoteConfig() {
+  const { template, fetchNewTemplate } = useTemplate();
+  const templateUpdater = useTemplateUpdater();
+
+  async function updateTemplate() {
+    // generate an updated template for testing
+    const newTemplate = JSON.parse(JSON.stringify(template));
+    newTemplate.parameters['welcome_message'].defaultValue.value =
+      'I am a new template';
+
+    try {
+      await templateUpdater(newTemplate);
+      fetchNewTemplate();
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  return (
+    <>
+      <button onClick={updateTemplate}>Update template</button>
+      <pre>{JSON.stringify(template, null, 2)}</pre>
+    </>
+  );
+}
+
+export default function RemoteConfigWrapper() {
+  return (
+    <Suspense fallback={'loading...'}>
+      <RemoteConfig />
+    </Suspense>
+  );
 }
