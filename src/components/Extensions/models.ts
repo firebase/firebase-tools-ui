03dec9ea969973e9aa2d21ca15f46e5a3dfe961a
TJ Lavelle <tlavelle@google.com>
2022-02-24 11:34:53 -0500

    Shared extension models (#716)
    * Shared extension models
    
    Inspired from firebase-tools and console
    
    * Add header and fix lock file

---
+++ src/components/Extensions/models.ts
@@ -0,0 +1,172 @@
+/**
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export type ExtensionName = `publishers/${string}/extensions/${string}`;
+
+export type ExtensionRef = `${string}/${string}`;
+
+export interface Extension {
+  name: ExtensionName;
+  ref: ExtensionRef;
+  state: 'STATE_UNSPECIFIED' | 'PUBLISHED' | 'DEPRECATED';
+  spec: ExtensionSpec;
+  hash: string;
+  sourceDownloadUri: string;
+  releaseNotes?: string;
+  createTime?: string;
+  deprecationMessage?: string;
+}
+
+export type ExtensionVersionName = `${ExtensionName}/versions/${string}`;
+
+export type ExtensionVersionRef = `${ExtensionRef}@${string}`;
+
+export interface ExtensionVersion {
+  name: ExtensionVersionName;
+  ref: ExtensionVersionRef;
+  state: 'STATE_UNSPECIFIED' | 'PUBLISHED' | 'DEPRECATED';
+  spec: ExtensionSpec;
+  hash: string;
+  sourceDownloadUri: string;
+  releaseNotes?: string;
+  createTime?: string;
+  deprecationMessage?: string;
+}
+
+export type ExtensionInstanceName = `projects/${string}/instances/${string}`;
+
+export interface ExtensionInstance {
+  name: ExtensionInstanceName;
+  createTime: string;
+  updateTime: string;
+  state:
+    | 'STATE_UNSPECIFIED'
+    | 'DEPLOYING'
+    | 'UNINSTALLING'
+    | 'ACTIVE'
+    | 'ERRORED'
+    | 'PAUSED';
+  config: ExtensionInstanceConfig;
+  serviceAccountEmail: string;
+  errorStatus?: string;
+  lastOperationName?: string;
+  lastOperationType?: string;
+  extensionRef?: string;
+  extensionVersion?: string;
+}
+
+export type ExtensionInstanceConfigName = `${ExtensionInstanceName}/configs/${string}`;
+
+export interface ExtensionInstanceConfig {
+  name: ExtensionInstanceConfigName;
+  createTime: string;
+  source: ExtensionSource;
+  params: {
+    [key: string]: any;
+  };
+  populatedPostinstallContent?: string;
+  extensionRef?: string;
+  extensionVersion?: string;
+}
+
+export type ExtensionSourceName = `projects/${string}/sources/${string}`;
+
+export interface ExtensionSource {
+  name: ExtensionSourceName;
+  state: 'STATE_UNSPECIFIED' | 'ACTIVE' | 'DELETED';
+  packageUri: string;
+  hash: string;
+  spec: ExtensionSpec;
+  extensionRoot?: string;
+  fetchTime?: string;
+  lastOperationName?: string;
+}
+
+export interface ExtensionSpec {
+  specVersion?: string;
+  name: string;
+  version: string;
+  displayName?: string;
+  description?: string;
+  apis?: Api[];
+  roles?: Role[];
+  resources: Resource[];
+  billingRequired?: boolean;
+  author?: Author;
+  contributors?: Author[];
+  license?: string;
+  releaseNotesUrl?: string;
+  sourceUrl: string;
+  params: Param[];
+  preinstallContent?: string;
+  postinstallContent?: string;
+  readmeContent?: string;
+  externalServices?: ExternalService[];
+}
+
+export interface ExternalService {
+  name: string;
+  pricingUri: string;
+}
+
+export interface Api {
+  apiName: string;
+  reason: string;
+}
+
+export interface Role {
+  role: string;
+  reason: string;
+}
+
+export interface Resource {
+  name: string;
+  type: string;
+  description?: string;
+  properties?: { [key: string]: any };
+  propertiesYaml?: string;
+}
+
+export interface Author {
+  authorName: string;
+  url?: string;
+}
+
+export interface Param {
+  param: string;
+  label: string;
+  description?: string;
+  default?: string;
+  type?: ParamType;
+  options?: ParamOption[];
+  required?: boolean;
+  validationRegex?: string;
+  validationErrorMessage?: string;
+  immutable?: boolean;
+  example?: string;
+}
+
+export enum ParamType {
+  STRING = 'STRING',
+  SELECT = 'SELECT',
+  MULTISELECT = 'MULTISELECT',
+  SECRET = 'SECRET',
+}
+
+export interface ParamOption {
+  value: string;
+  label?: string;
+}
