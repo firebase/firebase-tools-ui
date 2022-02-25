257a926c11773a536b0f386fa7826a485695837c
TJ Lavelle <tlavelle@google.com>
2022-02-25 15:08:30 -0500

    Fix license

--- src/components/Extensions/testing/TestExtensionsProvider.tsx
+++ src/components/Extensions/testing/TestExtensionsProvider.tsx
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import { MemoryRouter } from 'react-router-dom';
 
 import { Config } from '../../../store/config/types';
@@ -21,7 +37,7 @@ export const TestExtensionsProvider: React.FC<{
   return (
     <MemoryRouter initialEntries={[pagePath]}>
       <TestEmulatorConfigProvider config={emulatorConfig}>
-        <ExtensionsProvider extensionBackends={extensions}>
+        <ExtensionsProvider extensions={extensions}>
           {children}
         </ExtensionsProvider>
       </TestEmulatorConfigProvider>
