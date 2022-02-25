9193d11c13f4d24408ffb95e692f9f512af3cede
TJ Lavelle <tlavelle@google.com>
2022-02-25 16:39:21 -0500

    Add basic navigation for extensions list+details (#720)
    * Basic list and details navigation
    
    * Consolidate useExtension hook
    
    redirect when id doesn't match a known backend
    
    * Refactor apis to use context providers
    
    This results in:
    simpler tests - just provide w/ value
    simpler UI - just look at the context
    
    * also beefed up routes - only `routes` knows about magic string values
    
    * Fix license
    
    * Fix typo
    
    * Remove unused Link
    
    * Add type-guards for extension-types
    
    * Move routing to index

---
+++ src/components/Extensions/api/useExtensions.tsx
@@ -0,0 +1,65 @@
+/**
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { createContext, useContext } from 'react';
+
+import { Extension, ExtensionSpec, ExtensionVersion } from '../models';
+
+interface CommonExtensionBackend {
+  env: Record<string, string>;
+  extensionInstanceId: string;
+}
+
+interface PublishedExtensionBackend extends CommonExtensionBackend {
+  extension: Extension;
+  extensionVersion: ExtensionVersion;
+}
+
+interface LocalExtensionBackend extends CommonExtensionBackend {
+  extensionSpec: ExtensionSpec;
+}
+
+export type ExtensionBackend =
+  | PublishedExtensionBackend
+  | LocalExtensionBackend;
+
+export const ExtensionsContext = createContext<ExtensionBackend[]>([]);
+
+export const ExtensionsProvider: React.FC<{
+  extensionBackends: ExtensionBackend[];
+}> = ({ children, extensionBackends }) => {
+  return (
+    <ExtensionsContext.Provider value={extensionBackends}>
+      {children}
+    </ExtensionsContext.Provider>
+  );
+};
+
+export function useExtensions() {
+  return useContext(ExtensionsContext);
+}
+
+export function isPublishedExtension(
+  backend: ExtensionBackend
+): backend is PublishedExtensionBackend {
+  return backend.hasOwnProperty('extension');
+}
+
+export function isLocalExtension(
+  backend: ExtensionBackend
+): backend is LocalExtensionBackend {
+  return backend.hasOwnProperty('extensionSpec');
+}
