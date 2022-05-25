acef1bdfb9dccb84e42747a30bba0a9e95b2abeb
TJ Lavelle <tlavelle@google.com>
2022-05-24 20:07:26 -0400

    Update src/components/Storage/testing/renderWithStorage.tsx

--- src/components/Storage/testing/renderWithStorage.tsx
+++ src/components/Storage/testing/renderWithStorage.tsx
@@ -49,7 +49,7 @@ export async function renderWithStorage(children: ReactElement) {
   }
 
   const deleteAllFiles = async () => {
-    await waitFor(() => current.storage.toBeDefined());
+    await waitFor(() => !!current.storage);
 
     await act(async () => {
       await current.storage.deleteAllFiles();
