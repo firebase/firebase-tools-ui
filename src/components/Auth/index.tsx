05d5f0786c3046d6ec230d16d19a2cffa99fe5cd
Lisa Jian <lisajian@google.com>
2022-06-01 10:53:14 -0700

    Remove passthrough mode references

--- src/components/Auth/index.tsx
+++ src/components/Auth/index.tsx
@@ -33,7 +33,6 @@ import { EmulatorDisabled } from '../common/EmulatorDisabled';
 import { Spinner } from '../common/Spinner';
 import styles from './index.module.scss';
 import OneAccountPerEmailCard from './OneAccountPerEmailCard/OneAccountPerEmailCard';
-import PassthroughModeCard from './PassthroughModeCard/PassthroughModeCard';
 import ClearAll from './UsersCard/ClearAll';
 import TenantPicker, { useTenantFromUrl } from './UsersCard/TenantPicker';
 import UsersCard from './UsersCard/UsersCard';
@@ -89,9 +88,6 @@ export const Auth: React.FC<AuthProps> = ({ updateAuthConfig }) => {
       </Elevation>
       <Elevation z="2" wrap>
         <OneAccountPerEmailCard />
-      </Elevation>
-      <Elevation z="2" wrap>
-        <PassthroughModeCard />
       </Elevation>
     </GridCell>
   );
