4e2a49beb74ce703cdc493e947cf2fa841748e77
Jeff Huang <huangjeff@google.com>
2022-06-01 12:55:43 -0400

    Address comments

--- src/components/Extensions/Details/DetailCard/Tabs/EventsConfig.tsx
+++ src/components/Extensions/Details/DetailCard/Tabs/EventsConfig.tsx
@@ -27,10 +27,8 @@ export function EventsConfig() {
   const extension = useExtension()!;
   const eventsByPrefix: Map<string, string[]> = new Map(); // maps prefix ==> list of types
   const eventDescriptionMapping: Map<string, string> = new Map(); // maps type ==> description
-  let allowedEventTypesDiv;
-  let channelLocationDiv;
   !!extension.events &&
-    extension.events.forEach((event) => {
+    extension.events?.forEach((event) => {
       eventDescriptionMapping.set(event.type, event.description);
     });
   if (!!extension.allowedEventTypes && extension.allowedEventTypes.length > 0) {
@@ -42,83 +40,84 @@ export function EventsConfig() {
         eventsByPrefix.get(prefix)?.push(eventType);
       }
     });
-    const allowedEventTypeElements = Array.from(eventsByPrefix.keys())
-      .sort()
-      .map((prefix) => {
-        const events = eventsByPrefix.get(prefix)?.map((event) => {
-          return (
-            <div key={`${event}`} className={styles.eventValue}>
-              <ParamValue value={event} type={ParamType.STRING} />
-              <Typography use="body1" theme="secondary">
-                {eventDescriptionMapping.get(event) || 'Not Specified'}
-              </Typography>
-            </div>
-          );
-        });
+  }
+  const allowedEventTypeElements = Array.from(eventsByPrefix.keys())
+    .sort()
+    .map((prefix) => {
+      const events = eventsByPrefix.get(prefix)?.map((event) => {
         return (
-          <div key={`${prefix}`} className={styles.eventPrefixValue}>
-            <ParamValue value={prefix} type={ParamType.STRING} />
-            {events}
+          <div key={`${event}`} className={styles.eventValue}>
+            <ParamValue value={event} type={ParamType.STRING} />
+            <Typography use="body1" theme="secondary">
+              {eventDescriptionMapping.get(event) ?? 'Not Specified'}
+            </Typography>
           </div>
         );
       });
-    allowedEventTypesDiv = (
-      <div key="allowedEventTypes" className={styles.paramWrapper}>
-        <Accordion
-          expansionLabel="Description"
-          title={
-            <Typography use="body2" theme="secondary">
-              Types of events
-            </Typography>
-          }
-        >
-          <Typography
-            className={styles.description}
-            use="caption"
-            tag="div"
-            theme="secondary"
-          >
-            <Markdown>
-              Which events do you want to be emitted from this extension? Only
-              selected events will be published to Eventarc, all other events
-              will be ignored and will not be counted against Eventarc usage.
-            </Markdown>
+      return (
+        <div key={`${prefix}`} className={styles.eventPrefixValue}>
+          <ParamValue value={prefix} type={ParamType.STRING} />
+          {events}
+        </div>
+      );
+    });
+  const allowedEventTypesDiv = (!!extension.allowedEventTypes &&
+    extension.allowedEventTypes.length > 0) ?? (
+    <div key="allowedEventTypes" className={styles.wrapper}>
+      <Accordion
+        expansionLabel="Description"
+        title={
+          <Typography use="body2" theme="secondary">
+            Types of events
           </Typography>
-        </Accordion>
-        {allowedEventTypeElements}
-      </div>
-    );
-  }
-  if (!!extension.eventarcChannel) {
-    const location = extension.eventarcChannel.split('/')[3];
-    channelLocationDiv = (
-      <div key="channelLocation" className={styles.paramWrapper}>
-        <Accordion
-          expansionLabel="Description"
-          title={
-            <Typography use="body2" theme="secondary">
-              Channel location
-            </Typography>
-          }
+        }
+      >
+        <Typography
+          className={styles.description}
+          use="caption"
+          tag="div"
+          theme="secondary"
         >
-          <Typography
-            className={styles.description}
-            use="caption"
-            tag="div"
-            theme="secondary"
-          >
-            <Markdown>
-              Where do you want to deploy the Eventarc channel where events will
-              be published? You usually want a location close to where your
-              extension is deployed, but choosing the default location
-              (us-central1) makes it easier to create event handler functions.
-            </Markdown>
+          <Markdown>
+            Which events do you want to be emitted from this extension? Only
+            selected events will be published to Eventarc, all other events will
+            be ignored and will not be counted against Eventarc usage.
+          </Markdown>
+        </Typography>
+      </Accordion>
+      {allowedEventTypeElements}
+    </div>
+  );
+  const channelLocationDiv = !!extension.eventarcChannel ?? (
+    <div key="channelLocation" className={styles.paramWrapper}>
+      <Accordion
+        expansionLabel="Description"
+        title={
+          <Typography use="body2" theme="secondary">
+            Channel location
           </Typography>
-        </Accordion>
-        <ParamValue value={location} type={ParamType.STRING} />
-      </div>
-    );
-  }
+        }
+      >
+        <Typography
+          className={styles.description}
+          use="caption"
+          tag="div"
+          theme="secondary"
+        >
+          <Markdown>
+            Where do you want to deploy the Eventarc channel where events will
+            be published? You usually want a location close to where your
+            extension is deployed, but choosing the default location
+            (us-central1) makes it easier to create event handler functions.
+          </Markdown>
+        </Typography>
+      </Accordion>
+      <ParamValue
+        value={extension.eventarcChannel?.split('/')[3] ?? ''}
+        type={ParamType.STRING}
+      />
+    </div>
+  );
   return (
     <div>
       <div className={styles.wrapper}>
