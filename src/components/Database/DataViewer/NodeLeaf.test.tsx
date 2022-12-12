18064053ae42cc95cf61586eec5c795e6ea998c2
Tyler Stark <theirondeveloper@gmail.com>
2022-12-12 13:42:06 -0600

    fix(a11y) Add ability to focus on sub icon fields of RTDB (#889)
    This commit does 2 things:
    
    1. Add ability to focus on the sub icons (add/edit text)
    1. Updates the aria-label + tooltip text to be contextually descriptive
    
    FIXED=259452995, b/259452995
    b/259452995

--- src/components/Database/DataViewer/NodeLeaf.test.tsx
+++ src/components/Database/DataViewer/NodeLeaf.test.tsx
@@ -71,7 +71,7 @@ describe('editing the node', () => {
         Promise.resolve(<NodeLeaf realtimeRef={ref(db)} value={null} />)
       );
 
-      act(() => getByLabelText('Edit value').click());
+      act(() => getByLabelText('Edit value for: "null"').click());
 
       expect((getByLabelText('Field') as HTMLInputElement).value).toMatch(
         /^http:\/\/(localhost|127.0.0.1)/
@@ -83,7 +83,7 @@ describe('editing the node', () => {
         Promise.resolve(<NodeLeaf realtimeRef={ref(db)} value={null} />)
       );
 
-      act(() => getByLabelText('Edit value').click());
+      act(() => getByLabelText('Edit value for: "null"').click());
 
       expect((getByLabelText('Value') as HTMLInputElement).value).toBe('');
     });
@@ -95,7 +95,7 @@ describe('editing the node', () => {
         Promise.resolve(<NodeLeaf realtimeRef={ref(db)} value={null} />)
       );
 
-      act(() => getByLabelText('Edit value').click());
+      act(() => getByLabelText('Edit value for: "null"').click());
 
       expect((getByLabelText('Field') as HTMLInputElement).value).toMatch(
         /^http:\/\/(localhost|127.0.0.1)/
@@ -109,7 +109,7 @@ describe('editing the node', () => {
         )
       );
 
-      act(() => getByLabelText('Edit value').click());
+      act(() => getByLabelText('Edit value for: "foo"').click());
 
       expect((getByLabelText('Value') as HTMLInputElement).value).toBe(
         'my-value'
