c7dcd0cc741fea1547474aef1c0f15c477e78b58
Chris Thompson <christhompson@google.com>
2024-01-08 18:00:43 -0800

    setupTests fix for missing TextDecoder and TextEncoder

--- src/setupTests.js
+++ src/setupTests.js
@@ -13,7 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import { TextEncoder, TextDecoder } from 'util';
 
+Object.assign(global, { TextDecoder, TextEncoder });
 require('mutationobserver-shim');
 
 // reactFire has an implicit dependency on globalThis
