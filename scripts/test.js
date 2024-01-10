689cd2f7d4d93bae5f643c8857c0f62885c797ca
Chris Thompson <christhompson@google.com>
2024-01-09 17:54:29 -0800

    Lint

--- scripts/test.js
+++ scripts/test.js
@@ -24,7 +24,7 @@ process.env.PUBLIC_URL = '';
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
 // terminate the Node.js process with a non-zero exit code.
-process.on('unhandledRejection', err => {
+process.on('unhandledRejection', (err) => {
   throw err;
 });
 
@@ -63,6 +63,5 @@ if (
   const hasSourceControl = isInGitRepository() || isInMercurialRepository();
   argv.push(hasSourceControl ? '--watch' : '--watchAll');
 }
-
 
 jest.run(argv);
