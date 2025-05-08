6cf5453fec05e77ba4a99c2b4c3ac650bbd20962
Chris Thompson <christhompson@google.com>
2025-05-07 18:25:15 -0700

    Cleanup non-workflow files

--- vite.config.ts
+++ vite.config.ts
@@ -15,11 +15,11 @@
  */
 
 import { viteCommonjs } from '@originjs/vite-plugin-commonjs';
-import viteReact from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react';
 import { UserConfig, defineConfig } from 'vite';
-import { checker } from 'vite-plugin-checker';
+import checker from 'vite-plugin-checker';
 import { VitePluginNode } from 'vite-plugin-node';
-import  svgrPlugin  from 'vite-plugin-svgr';
+import svgrPlugin from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
@@ -32,7 +32,7 @@ export default defineConfig(({ command, mode }) => {
     },
     publicDir: mode === 'server' ? false : 'public',
     plugins: [
-      viteReact(),
+      react(),
       svgrPlugin({
         svgrOptions: {
           icon: true,
